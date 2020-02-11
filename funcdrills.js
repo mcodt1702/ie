ce24baef068e904c4024eeaa4a8c5e40f2905be9
Marco Coronado <marco@mcnyc.net>
2020-02-10 21:47:40 -0500

    step 6 commit

--- funcdrills.js
+++ funcdrills.js
@@ -1,5 +1,7 @@
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I'm ${age} year old`;
+  const yearOfBirth = 2020 - age;
+  return `Hi, my name is ${name} and I'm ${age} year old I was born in ${yearOfBirth}`;
 }
+
 const greeting1 = createGreeting();
 console.log(greeting1(marco, 34));
