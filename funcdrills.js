4cac2d5dc7cbdc2ef6144add1a9c9ef25be89aa7
mcodt1702 <marco@mcnyc.net>
2020-02-15 14:51:27 -0500

    modifications at library

--- funcdrills.js
+++ funcdrills.js
@@ -1,7 +1,35 @@
-function createGreeting(name, age) {
-  const yearOfBirth = 2020 - age;
-  return `Hi, my name is ${name} and I'm ${age} year old I was born in ${yearOfBirth}`;
-}
+// function createGreeting(name, age) {
+//   const yearOfBirth = 2020 - age;
+//   function yearOfBirth(age){
+//     if (yearOfBirth<=0){
+//       throw new Error("Age can not be negative");}
 
-const greeting1 = createGreeting();
-console.log(greeting1(marco, 34));
+//    return `Hi, my name is ${name} and I'm ${age} year old I was born in ${yearOfBirth}`;
+// }
+
+// //const greeting1 = createGreeting();
+// console.log(createGreeting('marco', -34));
+function createGreeting(name, age) {
+        function yearOfBirth(age) {
+          if (name === undefined || age === undefined) {
+            throw new Error('Arguments not valid.');
+          }
+                     if(age<=0){
+                      throw new Error('Age can not be negative')
+                     } 
+                     
+                     if (typeof age !== 'number') {
+                      throw new TypeError('Age must be a number');
+                    }
+                     else {const yob = 2020 - age;
+                      return yob; 
+                     }   
+                
+                          };
+
+  console.log(yearOfBirth(age));
+
+  return `Hi, my name is ${name} im ${age}`;
+}
+const greeting1 = createGreeting("Marco", 49);
+console.log(greeting1);
